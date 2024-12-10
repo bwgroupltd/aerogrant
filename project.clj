576b8e6829e7b4abd727355184634f7ab19f55fc
Simon Robinson <simon.robinson@soundunited.com>
2024-12-10 15:46:24 +0000

    change project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.bigsy/aerogrant "0.1.12-SNAPSHOT"
+(defproject audiogum/aerogrant "0.1.13-SNAPSHOT"
   :description "mashup of aero and integrant with some nice addons"
   :url "https://github.com/Bigsy/aerogrant"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
